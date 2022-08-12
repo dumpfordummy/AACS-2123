64a231d8d97c24be65bdd727e2c82ef24023dce0
dumpfordummy <87067231+dumpfordummy@users.noreply.github.com>
2022-08-12 11:13:05 +0800

    first commit

---
+++ ROYAL TD/Assets/Assets github/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1;
+    public Animator animator;
+    private float horizontalInput = 0;
+    private float verticalInput = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        Debug.Log("Horizontal :" + horizontalInput);
+        Debug.Log("Vertical :" + verticalInput);
+
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime);
+
+        if (horizontalInput > 0)
+            transform.localScale = new Vector3(-1, 1, 0);
+        else if (horizontalInput < 0)
+            transform.localScale = new Vector3(1, 1, 0);
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Debug.Log("Attack");
+            animator.SetBool("Attack", true);
+        }
+        if (Input.GetKeyUp(KeyCode.H))
+        {
+            animator.SetBool("Attack", false);
+        }
+    }
+}
