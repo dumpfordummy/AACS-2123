41de57b6271571e65523a07da6068aff747c3271
dumpfordummy <87067231+dumpfordummy@users.noreply.github.com>
2022-09-02 10:35:31 +0800

    Update Weapon.cs

--- ROYAL TD/Assets/Assets github/Scripts/Weapon.cs
+++ ROYAL TD/Assets/Assets github/Scripts/Weapon.cs
@@ -16,62 +16,57 @@ public class Weapon : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         animator = GetComponent<Animator>();
-        if(timeRemaining > 0)
+        if (timeRemaining > 0)
         {
             timeRemaining -= Time.deltaTime;
         }
         else
         {
             SetAnimations(overrideControllers[0]);
-            
+
         }
         Attack();
     }
 
     public void Attack()
     {
-        if(!Input.GetKey(KeyCode.Alpha2))
+        if (!Input.GetKey(KeyCode.Alpha2))
         {
             return;
         }
 
-        if(direction == 3)
+        timeRemaining = 1;
+        GetComponentInParent<Stone>().receiveDamage(30);
+
+        if (direction == 3)
         {
             GetComponent<SpriteRenderer>().sortingOrder = 0;
-            timeRemaining = 1;
             SetAnimations(overrideControllers[1]);
-            GetComponentInParent<Stone>().receiveDamage(30);
         }
 
         if (direction == 1)
         {
             GetComponent<SpriteRenderer>().sortingOrder = -1;
-            timeRemaining = 1;
             SetAnimations(overrideControllers[3]);
-            GetComponentInParent<Stone>().receiveDamage(30);
         }
 
         if (direction == 2)
         {
             GetComponent<SpriteRenderer>().sortingOrder = 0;
-            timeRemaining = 1;
             SetAnimations(overrideControllers[2]);
-            GetComponentInParent<Stone>().receiveDamage(30);
         }
 
         if (direction == 4)
         {
             GetComponent<SpriteRenderer>().sortingOrder = 0;
-            timeRemaining = 1;
             SetAnimations(overrideControllers[2]);
-            GetComponentInParent<Stone>().receiveDamage(30);
         }
     }
 
