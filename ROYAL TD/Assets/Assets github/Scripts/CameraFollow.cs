ffe999576cce35aab6ba250b027de0096f6ccee9
dumpfordummy <87067231+dumpfordummy@users.noreply.github.com>
2022-09-02 13:00:56 +0800

    Update CameraFollow.cs

--- ROYAL TD/Assets/Assets github/Scripts/CameraFollow.cs
+++ ROYAL TD/Assets/Assets github/Scripts/CameraFollow.cs
@@ -9,6 +9,7 @@ public class CameraFollow : MonoBehaviour
     internal static Transform target;
     public float lerpSpeed = 1.0f;
 
+    private GameObject player;
     private Vector3 offset;
 
     private Vector3 targetPos;
@@ -22,11 +23,15 @@ public class CameraFollow : MonoBehaviour
 
     private void Update()
     {
-        if (target == null) return;
+        if ((player = GameObject.FindGameObjectWithTag("Player")) != null)
+        {
+            target = player.GetComponent<Transform>();
+            targetPos = target.position + offset;
+            Vector3 lerp = Vector3.Lerp(transform.position, targetPos, lerpSpeed * Time.deltaTime);
+            transform.position = new Vector3(lerp.x, lerp.y, -10);
+        }
 
-        targetPos = target.position + offset;
-        Vector3 lerp = Vector3.Lerp(transform.position, targetPos, lerpSpeed * Time.deltaTime);
-        transform.position = new Vector3(lerp.x, lerp.y, -10);
+        
     }
 
 }
