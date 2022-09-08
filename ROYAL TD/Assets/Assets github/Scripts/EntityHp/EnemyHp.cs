fa104ac67d05f65b3cc192b2d72965245a4fc1c6
CKY25 <ckyy0625@gmail.com>
2022-09-08 19:44:38 +0800

    added enemy health

--- ROYAL TD/Assets/Assets github/Scripts/EntityHp/EnemyHp.cs
+++ ROYAL TD/Assets/Assets github/Scripts/EntityHp/EnemyHp.cs
@@ -5,16 +5,27 @@ using UnityEngine.UI;
 
 public class EnemyHp : MonoBehaviour
 {
-    [SerializeField] public float hp;
+    [SerializeField] public float maxHp = 10000;
+    [SerializeField] public float currentHp = 10000;
+
+    public EnemyHpBar healthBar;
+
+    private void Start()
+    {
+        currentHp = maxHp;
+        healthBar.setMaxHealth(maxHp);
+    }
 
     public void DecreaseEntityHp(float hpToDecrease)
     {
-        hp -= hpToDecrease;
+        currentHp -= hpToDecrease;
 
-        if (hp <= 0)
+        if (currentHp <= 0)
         {
             Destroy(gameObject);
         }
+
+        healthBar.setHealth(currentHp);
     }
 }
 
--- ROYAL TD/Assets/Assets github/Scripts/EntityHp/EnemyHpBar.cs
+++ ROYAL TD/Assets/Assets github/Scripts/EntityHp/EnemyHpBar.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHpBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Slider slider;
+
+    public void setMaxHealth(float health)
     {
-        
+        slider.maxValue = health;
+        slider.value = health;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void setHealth(float health)
     {
-        
+        slider.value = health;
     }
 }
