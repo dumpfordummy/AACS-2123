3af5fd7519bb8bf83ccb46c6f556f135e7d78214
dumpfordummy <87067231+dumpfordummy@users.noreply.github.com>
2022-09-08 20:05:05 +0800

    Update EnemyHp.cs

--- ROYAL TD/Assets/Assets github/Scripts/EntityHp/EnemyHp.cs
+++ ROYAL TD/Assets/Assets github/Scripts/EntityHp/EnemyHp.cs
@@ -7,23 +7,31 @@ public class EnemyHp : MonoBehaviour
 {
     [SerializeField] public float maxHp = 10000;
     [SerializeField] public float currentHp = 10000;
-
     public EnemyHpBar healthBar;
+    private Enemy enemy;
 
     private void Start()
     {
         currentHp = maxHp;
         healthBar.setMaxHealth(maxHp);
+        enemy = GetComponent<Enemy>();
     }
 
     public void DecreaseEntityHp(float hpToDecrease)
     {
+        if(enemy.target == null)
+        {
+            return;
+        }
+
         currentHp -= hpToDecrease;
 
         if (currentHp <= 0)
         {
             Destroy(gameObject);
         }
+
+
 
         healthBar.setHealth(currentHp);
     }
