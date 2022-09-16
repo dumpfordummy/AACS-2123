3a849be35701365635e6f204ac881f263b51ae23
CKY25 <ckyy0625@gmail.com>
2022-09-16 11:22:44 +0800

    test

--- ROYAL TD/Assets/Assets github/Scripts/SoundController.cs
+++ ROYAL TD/Assets/Assets github/Scripts/SoundController.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class SoundController : MonoBehaviour
 {
     public AudioSource sound;
-    private GameObject enemy;
     public AudioClip batClip;
+    public AudioClip crabClip;
 
     void Update()
     {
-        if (enemy == GameObject.Find("Bat"))
+        if (GameObject.Find("Bat"))
         {
             GetComponent<AudioSource>().clip = batClip;
+            if (!sound.isPlaying)
+            {
+                sound.Play();
+            }
+        }
+        else if (GameObject.Find("Crab"))
+        {
+            GetComponent<AudioSource>().clip = crabClip;
             if (!sound.isPlaying)
             {
                 sound.Play();
