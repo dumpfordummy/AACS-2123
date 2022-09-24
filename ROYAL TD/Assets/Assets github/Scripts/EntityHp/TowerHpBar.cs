a99962a035035ce42806e7519bb39c51789df56c
CKY25 <ckyy0625@gmail.com>
2022-09-24 15:39:35 +0800

    added health bar gradient

--- ROYAL TD/Assets/Assets github/Scripts/EntityHp/TowerHpBar.cs
+++ ROYAL TD/Assets/Assets github/Scripts/EntityHp/TowerHpBar.cs
@@ -6,15 +6,21 @@ using UnityEngine.UI;
 public class TowerHpBar : MonoBehaviour
 {
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
 
     public void setMaxHealth(float health)
     {
         slider.maxValue = health;
         slider.value = health;
+
+        fill.color = gradient.Evaluate(1f);
     }
 
     public void setHealth(float health)
     {
         slider.value = health;
+
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
