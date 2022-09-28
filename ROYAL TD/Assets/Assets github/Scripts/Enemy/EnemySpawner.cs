7b4b4af4358d364467deba82cdd9df714ddb3b3b
dumpfordummy <87067231+dumpfordummy@users.noreply.github.com>
2022-09-28 18:52:30 +0800

    Update EnemySpawner.cs

--- ROYAL TD/Assets/Assets github/Scripts/Enemy/EnemySpawner.cs
+++ ROYAL TD/Assets/Assets github/Scripts/Enemy/EnemySpawner.cs
@@ -270,6 +270,8 @@ public class EnemySpawner : MonoBehaviour
             //GameObject.Instantiate(enemy, new Vector3(Random.Range(-40, 50) - 0.5f, Random.Range(15, 25) - 0.5f, 0), new Quaternion());
             enemy.transform.position = new Vector3(Random.Range(-40, 50) - 0.5f, Random.Range(15, 25) - 0.5f);
         }
+
+        enemy.GetComponent<EnemyMovement>().SetWaypoint(); // initialize enemy pathfinding
     }
 
     private GameObject FindEnemy(int n)
